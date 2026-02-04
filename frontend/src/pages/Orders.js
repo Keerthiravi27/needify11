3666d0308dce5a0dde47c61fb63ff82a83619a94
Keerthiravi27 <ravichandrankeerthi2007@gmail.com>
2026-02-04 13:52:29 +0000

    auto-commit for 7db8d524-1e3d-489e-99e7-c845d3bf2753

--- frontend/src/pages/Orders.js
+++ frontend/src/pages/Orders.js
@@ -6,7 +6,7 @@ import { BottomNav } from '../components/BottomNav';
 import { Button } from '../components/ui/button';
 import { RazorpayPaymentModal } from '../components/RazorpayPayment';
 import { AlertDialog, AlertDialogAction, AlertDialogCancel, AlertDialogContent, AlertDialogDescription, AlertDialogFooter, AlertDialogHeader, AlertDialogTitle, AlertDialogTrigger } from '../components/ui/alert-dialog';
-import { ClipboardList, Package, Banknote, XCircle } from 'lucide-react';
+import { ClipboardList, Package, Banknote, XCircle, MessageCircle } from 'lucide-react';
 import { toast } from 'sonner';
 
 const API_URL = process.env.REACT_APP_BACKEND_URL + '/api';
